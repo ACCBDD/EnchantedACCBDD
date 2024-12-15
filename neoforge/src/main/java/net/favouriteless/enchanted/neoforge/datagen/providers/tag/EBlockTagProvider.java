0a77be4748d14276c4c14e8782cd7f73914b5552
Favouriteless <62647254+Favouriteless@users.noreply.github.com>
2024-12-15 18:05:58 +0000

    Fixed datagen tags

--- neoforge/src/main/java/net/favouriteless/enchanted/neoforge/datagen/providers/tag/EBlockTagProvider.java
+++ neoforge/src/main/java/net/favouriteless/enchanted/neoforge/datagen/providers/tag/EBlockTagProvider.java
@@ -16,6 +16,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
 
+@SuppressWarnings("unchecked")
 public class EBlockTagProvider extends IntrinsicHolderTagsProvider<Block> {
 
     public EBlockTagProvider(PackOutput output, CompletableFuture<Provider> lookupProvider, ExistingFileHelper existingFileHelper) {
@@ -34,54 +35,45 @@ public class EBlockTagProvider extends IntrinsicHolderTagsProvider<Block> {
         tag(ETags.Blocks.BLIGHT_DECAYABLE_BLOCKS)
                 .addTag(BlockTags.DIRT);
         tag(ETags.Blocks.BLIGHT_DECAYABLE_PLANTS)
-                .addTag(BlockTags.SAPLINGS)
-                .addTag(BlockTags.SMALL_FLOWERS)
+                .addTags(BlockTags.SAPLINGS, BlockTags.SMALL_FLOWERS)
                 .add(Blocks.SHORT_GRASS, Blocks.FERN, Blocks.SWEET_BERRY_BUSH)
                 .add(EBlocks.GLINT_WEED.get());
         tag(ETags.Blocks.CHALICES)
                 .add(EBlocks.CHALICE.get(), EBlocks.CHALICE_FILLED.get());
         tag(ETags.Blocks.CHALKS)
-                .add(EBlocks.GOLDEN_CHALK.get(), EBlocks.RITUAL_CHALK.get(),
-                        EBlocks.NETHER_CHALK.get(), EBlocks.OTHERWHERE_CHALK.get());
+                .add(EBlocks.GOLDEN_CHALK.get(), EBlocks.RITUAL_CHALK.get(), EBlocks.NETHER_CHALK.get(),
+                        EBlocks.OTHERWHERE_CHALK.get());
         tag(ETags.Blocks.CROPS)
-                .add(EBlocks.BELLADONNA.get(), EBlocks.SNOWBELL.get(), EBlocks.MANDRAKE.get(),
-                        EBlocks.GARLIC.get(), EBlocks.WOLFSBANE.get());
+                .add(EBlocks.BELLADONNA.get(), EBlocks.SNOWBELL.get(), EBlocks.MANDRAKE.get(), EBlocks.GARLIC.get(),
+                        EBlocks.WOLFSBANE.get());
         tag(ETags.Blocks.FENCES)
                 .addTag(ETags.Blocks.WOODEN_FENCES);
         tag(ETags.Blocks.FENCE_GATES)
-                .add(EBlocks.ALDER_FENCE_GATE.get(), EBlocks.HAWTHORN_FENCE_GATE.get(),
-                        EBlocks.ROWAN_FENCE_GATE.get());
+                .add(EBlocks.ALDER_FENCE_GATE.get(), EBlocks.HAWTHORN_FENCE_GATE.get(), EBlocks.ROWAN_FENCE_GATE.get());
         tag(ETags.Blocks.LEAVES)
-                .add(EBlocks.ALDER_LEAVES.get(), EBlocks.ROWAN_LEAVES.get(),
-                        EBlocks.HAWTHORN_LEAVES.get());
+                .add(EBlocks.ALDER_LEAVES.get(), EBlocks.ROWAN_LEAVES.get(), EBlocks.HAWTHORN_LEAVES.get());
         tag(ETags.Blocks.LOGS)
-                .add(EBlocks.ALDER_LOG.get(), EBlocks.ROWAN_LOG.get(),
-                        EBlocks.HAWTHORN_LOG.get());
+                .add(EBlocks.ALDER_LOG.get(), EBlocks.ROWAN_LOG.get(), EBlocks.HAWTHORN_LOG.get());
         tag(ETags.Blocks.MUTANDIS_BLACKLIST)
                 .add(Blocks.WITHER_ROSE)
                 .add(EBlocks.BLOOD_POPPY.get());
         tag(ETags.Blocks.MUTANDIS_EXTREMIS_PLANTS)
-                .addTag(ETags.Blocks.MUTANDIS_PLANTS)
-                .addTag(ETags.Blocks.CROPS)
+                .addTags(ETags.Blocks.MUTANDIS_PLANTS, ETags.Blocks.CROPS)
                 .addTag(BlockTags.CROPS)
                 .add(Blocks.SUGAR_CANE, Blocks.CACTUS)
                 .add(EBlocks.BLOOD_POPPY.get());
         tag(ETags.Blocks.MUTANDIS_PLANTS)
-                .addTag(BlockTags.SAPLINGS)
-                .addTag(BlockTags.SMALL_FLOWERS)
+                .addTags(BlockTags.SAPLINGS, BlockTags.SMALL_FLOWERS)
                 .add(Blocks.SHORT_GRASS, Blocks.BROWN_MUSHROOM, Blocks.RED_MUSHROOM)
-                .add(EBlocks.EMBER_MOSS.get(), EBlocks.GLINT_WEED.get(),
-                        EBlocks.SPANISH_MOSS.get());
+                .add(EBlocks.EMBER_MOSS.get(), EBlocks.GLINT_WEED.get(), EBlocks.SPANISH_MOSS.get());
         tag(ETags.Blocks.PLANKS)
-                .add(EBlocks.ROWAN_PLANKS.get(), EBlocks.ALDER_PLANKS.get(),
-                        EBlocks.HAWTHORN_PLANKS.get());
+                .add(EBlocks.ROWAN_PLANKS.get(), EBlocks.ALDER_PLANKS.get(), EBlocks.HAWTHORN_PLANKS.get());
         tag(ETags.Blocks.RITE_FOREST_REPLACEABLE)
                 .addTag(BlockTags.SMALL_FLOWERS)
                 .add(Blocks.FERN)
                 .add(EBlocks.GLINT_WEED.get());
         tag(ETags.Blocks.SAPLINGS)
-                .add(EBlocks.ROWAN_SAPLING.get(), EBlocks.ALDER_SAPLING.get(),
-                        EBlocks.HAWTHORN_SAPLING.get());
+                .add(EBlocks.ROWAN_SAPLING.get(), EBlocks.ALDER_SAPLING.get(), EBlocks.HAWTHORN_SAPLING.get());
         tag(ETags.Blocks.SLABS)
                 .addTag(ETags.Blocks.WOODEN_SLABS);
         tag(ETags.Blocks.STAIRS)
@@ -89,29 +81,27 @@ public class EBlockTagProvider extends IntrinsicHolderTagsProvider<Block> {
         tag(ETags.Blocks.TRANSPOSE_IRON)
                 .addTag(BlockTags.IRON_ORES);
         tag(ETags.Blocks.WOODEN_SLABS)
-                .add(EBlocks.ROWAN_SLAB.get(), EBlocks.ALDER_SLAB.get(),
-                        EBlocks.HAWTHORN_SLAB.get());
+                .add(EBlocks.ROWAN_SLAB.get(), EBlocks.ALDER_SLAB.get(), EBlocks.HAWTHORN_SLAB.get());
         tag(ETags.Blocks.WOODEN_FENCES)
-                .add(EBlocks.ROWAN_FENCE.get(), EBlocks.ALDER_FENCE.get(),
-                        EBlocks.HAWTHORN_FENCE.get());
+                .add(EBlocks.ROWAN_FENCE.get(), EBlocks.ALDER_FENCE.get(), EBlocks.HAWTHORN_FENCE.get());
         tag(ETags.Blocks.WOODEN_STAIRS)
-                .add(EBlocks.ROWAN_STAIRS.get(), EBlocks.ALDER_STAIRS.get(),
-                        EBlocks.HAWTHORN_STAIRS.get());
+                .add(EBlocks.ROWAN_STAIRS.get(), EBlocks.ALDER_STAIRS.get(), EBlocks.HAWTHORN_STAIRS.get());
     }
 
     public void addVanillaTags(Provider provider) {
         // Mineable
         tag(BlockTags.MINEABLE_WITH_AXE)
+                .addTags(ETags.Blocks.LOGS, ETags.Blocks.WOODEN_FENCES, ETags.Blocks.WOODEN_STAIRS,
+                ETags.Blocks.WOODEN_SLABS)
                 .add(EBlocks.SPINNING_WHEEL.get());
         tag(BlockTags.MINEABLE_WITH_HOE)
-                .addTag(ETags.Blocks.LOGS)
+                .addTag(ETags.Blocks.LEAVES)
                 .add(EBlocks.WICKER_BUNDLE.get());
         tag(BlockTags.MINEABLE_WITH_PICKAXE)
                 .addTag(ETags.Blocks.CHALICES)
-                .add(EBlocks.WITCH_OVEN.get(), EBlocks.FUME_FUNNEL.get(),
-                        EBlocks.FUME_FUNNEL_FILTERED.get(), EBlocks.WITCH_CAULDRON.get(),
-                        EBlocks.DISTILLERY.get(), EBlocks.KETTLE.get(), EBlocks.ALTAR.get(),
-                        EBlocks.POPPET_SHELF.get());
+                .add(EBlocks.WITCH_OVEN.get(), EBlocks.FUME_FUNNEL.get(), EBlocks.FUME_FUNNEL_FILTERED.get(),
+                        EBlocks.WITCH_CAULDRON.get(), EBlocks.DISTILLERY.get(), EBlocks.KETTLE.get(),
+                        EBlocks.ALTAR.get(), EBlocks.POPPET_SHELF.get());
 
         // Other tags
         tag(BlockTags.CROPS)
@@ -142,4 +132,5 @@ public class EBlockTagProvider extends IntrinsicHolderTagsProvider<Block> {
                 .addTag(ETags.Blocks.WOODEN_STAIRS);
 
     }
+
 }
